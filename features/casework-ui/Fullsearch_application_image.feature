a35c6d30946640335de31c1c884d0edfa2b9c42e
nicola <nicola.andrews@digital.landregisty.gov.uk>
2015-09-28 12:56:40 +0100

    sprint 7

---
+++ features/casework-ui/Fullsearch_application_image.feature
@@ -0,0 +1,39 @@
+@fsai
+
+Feature: search application image
+
+As a land charges caseworker 
+I want to be able to view an image of a bankruptcy full search request that has been posted to LR
+So that I can ensure that the search request details are captured and processed correctly
+
+Scenario: Using the bankruptcy searchs task on the  Land Charges system
+
+#SH-US018 Full search - View Postal Search
+Given I am on the bankruptcy searches screen
+When I select an application type of Full Search the application is displayed
+When the image of the full search application is displayed I can click on all available pages
+When I click on a page the image of the full search is visible
+When I am on a page I can zoom in
+Then I am on a page I can zoom out
+
+#SH-US019 - Full Search - Capture Customer Details
+Given I am on the bankruptcy full search details screen
+When I click on the name details tab I can enter six names
+When I click in the search to field I can add the current year
+When I click on the Customer details tab I can enter the key number Customer Name Customer Address Customer Reference
+When I click on entered details in the address box I can make an amendment
+When I click on the search areas tab all counties check box search area  List of Areas to search is displayed
+When I can click the complete search button when the customer address field is complete
+When the application has been submitted a confirmation screen is displayed
+When the user can return to the worklist
+When I select an application type of Full Search the application is displayed
+When I click on the name details tab I can enter six names
+When I click on the Customer details tab I can enter the key number Customer Name Customer Address Customer Reference
+When I click on entered details in the address box I can make an amendment
+When I click on the search areas tab all counties check box search area  List of Areas to search is displayed
+When I enter details into the search area edit box I can click on the add area button
+When add area button is clicked the search area details are added to the List of areas to search box
+When I click on entered details in the list of areas search box I can make an amendment
+When I can click the complete search button when the customer address field is complete
+When the application has been submitted a confirmation screen is displayed
+Then the user can return to the worklist
---
+++ features/step_definitions/fullsearch_application_image.rb
@@ -0,0 +1,66 @@
+When(/^I select an application type of Full Search the application is displayed$/) do
+  #apptype_search = find(:xpath, '//*[@id="results"]/tbody/tr[1]/td[3]').text
+ #apptype_fullsearch = find(:id, 'app_type2').text
+ #if app type == 'Search'
+  #then  find(:id, app_type =='Search').click
+  #find(:xpath,'html/body/div[1]/div/div/div[3]/div/table/tbody/tr[1]/td[1]').click
+  visit( "http://localhost:5010/get_list?appn=search" )
+    find(:id,'app_type2').click
+end
+
+When(/^I click on a page the image of the full search is visible$/) do
+  find(:id, 'thumbnail1').click
+end
+
+When(/^the image of the full search application is displayed I can click on all available pages$/) do
+  find(:id, 'thumbnail1').click
+end
+
+Given(/^I am on the bankruptcy full search details screen$/) do
+  expect(page).to have_content('Full Search')
+  expect(page).to have_content('Full Name')
+  expect(page).to have_content('Complete the debtor')
+  expect(page).to have_button('Complete Search')
+end
+
+When(/^I click on entered details in the address box I can make an amendment$/) do
+  fill_in('customer_address',:with => '21 long street' "\n"'London'"\n"'SW1 1BP')
+end
+
+When(/^I click on the search areas tab all counties check box search area  List of Areas to search is displayed$/) do
+  click_link('Search Areas')
+  check('all_counties')
+end
+
+When(/^I enter details into the search area edit box I can click on the add area button$/) do
+  fill_in('search_area',:with =>'Plymouth')
+  click_link('Add Area')
+  fill_in('search_area',:with =>'Saltash')
+  click_link('Add Area')
+end
+
+When(/^add area button is clicked the search area details are added to the List of areas to search box$/) do
+  #expect('area_list').to have_content('Plymouth')
+
+end
+
+When(/^I click on entered details in the list of areas search box I can make an amendment$/) do
+  fill_in('area_list',:with =>'Plymouth')
+end
+
+
+
+When(/^I click in the search to field I can add the current year$/) do
+  fill_in('year_from0',:with =>'1975')
+  fill_in('year_to0',:with =>'1976')
+  fill_in('year_from3',:with =>'1969')
+  fill_in('year_from4',:with =>'2000')
+end
+
+Then(/^I can click the complete search button when the customer address field is complete$/) do
+   click_button('Complete Search')
+end
+
+When(/^the application has been submitted a confirmation screen is displayed$/) do
+  expect(page).to have_content('Application Complete')
+end
