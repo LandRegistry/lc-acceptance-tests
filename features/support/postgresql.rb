fe4d38782ea923482868b0ed68205f14e6f1ed10
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-11-05 15:29:45 +0000

    Data unload/load

--- features/support/postgresql.rb
+++ features/support/postgresql.rb
@@ -1,12 +1,24 @@
 class PostgreSQL
   def self.connect(database)
-    @@pg = PGconn.connect($POSTGRES_HOST, $POSTGRES_PORT,  '', '', database, $POSTGRES_USER, $POSTGRES_PASSWORD)
+    user = ''
+    if database == "landcharges"
+      user = "landcharges"
+    elsif database == "working"
+      user = "lc-working-data"
+    elsif database == "db2"
+      user = "lc-db2-mock"
+    elsif database == "docstore"
+      user = "lc-documents"
+    end
+
+    puts "DB: #{database}; User: #{user}"
+    @@pg = PGconn.connect($POSTGRES_HOST, $POSTGRES_PORT,  '', '', database, user, "lcalpha")
   end
-  
+
   def self.disconnect
     @@pg.close
   end
-  
+
   def self.query(sql)
     @@pg.exec(sql)
   end
