6db9792996eb729ffb669c25d61293f72a371834
nicola <nicola.andrews@digital.landregisty.gov.uk>
2015-12-14 12:54:08 +0000

    document storage

--- features/casework-ui/document_storage.feature
+++ features/casework-ui/document_storage.feature
@@ -7,6 +7,8 @@ I want to be able to store retrieve amend an image
 So that images of application forms and associated documents may be retained
 
 Scenario: Document storage
+
+#DS-US002 ADD A DOCUMENT
 Given I have a document that has been scanned onto the system	
-When I use the system to view the scanned document	
-Then I access the DB2 table to confirm row exists
+When document is added retrieved deleted replaced the system returns a positive response code	
+When I access the DB2 table to confirm row exists
--- features/step_definitions/document_storage.rb
+++ features/step_definitions/document_storage.rb
@@ -1,39 +1,87 @@
 
 Given(/^I have a document that has been scanned onto the system$/) do
-  f = IO.binread('blank_amend.jpg')
-
-
-uri = URI("http://localhost:5014")
-http = Net::HTTP.new(uri.host, uri.port)
-
-# Save the image to the API
-request = Net::HTTP::Post.new('/forms/66/images')
-request['Content-Type'] = 'image/jpeg'
-request.body = f
-upload = Digest::SHA1.hexdigest(f)
-response = http.request(request)
-
-puts response.code
-
-# Retrieve the image
-get = Net::HTTP::Get.new('/forms/66/images/2')
-get_resp = http.request(get)
-
-download = Digest::SHA1.hexdigest(get_resp.body)
-
-puts get_resp.code
-File.open("output.jpg", 'wb' ) do |output|
-  output.write get_resp.body
-end
-
-
-puts "#{upload} was uploaded"
-puts "#{download} was downloaded"
+  #this is a given place holder
 
 end
 
-When(/^I use the system to view the scanned document$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^document is added retrieved deleted replaced the system returns a positive response code$/) do
+  f = IO.binread('test.tiff')
+
+
+  uri = URI("http://localhost:5014")
+  http = Net::HTTP.new(uri.host, uri.port)
+  
+  # Save the image to the API
+  request = Net::HTTP::Post.new('/forms/66/images')
+  request['Content-Type'] = 'image/tiff'
+  request.body = f
+  upload = Digest::SHA1.hexdigest(f)
+  response = http.request(request)
+  
+  puts response.code
+  expect(response.code).to eql "201"
+
+
+  # Retrieve the image
+  get = Net::HTTP::Get.new('/forms/66/images/2')
+  get_resp = http.request(get)
+  
+  expect(get_resp.code).to eql "200"
+
+  download = Digest::SHA1.hexdigest(get_resp.body)
+  
+  puts get_resp.code
+  File.open("output.jpg", 'wb' ) do |output|
+    output.write get_resp.body
+  end
+  
+  puts "#{upload} was uploaded"
+  puts "#{download} was downloaded"
+  
+  # Delete the image
+  get = Net::HTTP::Delete.new('/forms/66/images/2')
+  get_resp = http.request(get)
+  
+  expect(get_resp.code).to eql "404"
+
+  download = Digest::SHA1.hexdigest(get_resp.body)
+  
+  puts get_resp.code
+  File.open("output.jpg", 'wb' ) do |output|
+    output.write get_resp.body
+  end
+  
+  uri = URI("http://localhost:5014")
+  http = Net::HTTP.new(uri.host, uri.port)
+  
+  # Save the image to the API
+  request = Net::HTTP::Post.new('/forms/66/images')
+  request['Content-Type'] = 'image/tiff'
+  request.body = f
+  upload = Digest::SHA1.hexdigest(f)
+  response = http.request(request)
+  
+  puts response.code
+  expect(response.code).to eql "201"
+
+
+  # Retrieve the image
+  get = Net::HTTP::Get.new('/forms/66/images/2')
+  get_resp = http.request(get)
+  
+  expect(get_resp.code).to eql "200"
+
+  download = Digest::SHA1.hexdigest(get_resp.body)
+  
+  puts get_resp.code
+  File.open("output.jpg", 'wb' ) do |output|
+    output.write get_resp.body
+  end
+  
+  puts "#{upload} was uploaded"
+  puts "#{download} was downloaded"
+  
+
 end
 
 Then(/^I access the DB(\d+) table to confirm row exists$/) do |arg1|
