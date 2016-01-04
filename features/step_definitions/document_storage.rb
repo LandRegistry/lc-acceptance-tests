475cea323e79e4759f178ae7c924ff0804785f9c
User1 <user1@LRAdmins-MacBook-Pro.local>
2016-01-04 10:39:15 +0000

    image replacement etc

--- features/step_definitions/document_storage.rb
+++ features/step_definitions/document_storage.rb
@@ -19,12 +19,76 @@ When(/^document is added retrieved deleted replaced the system returns a positiv
   response = http.request(request)
   
   puts response.code
+  image = JSON.parse(response.body)
+  id = image['id']
   expect(response.code).to eql "201"
 
-  puts "#{upload} was uploaded"
+  puts "#{upload} was uploaded as a new document"
   
   
   # Retrieve the image
+  get = Net::HTTP::Get.new("/forms/#{id}/1")
+  get_resp = http.request(get)
+  
+  expect(get_resp.code).to eql "200"
+
+  download = Digest::SHA1.hexdigest(get_resp.body)
+  
+  puts get_resp.code
+  File.open("output.jpg", 'wb' ) do |output|
+    output.write get_resp.body
+  end
+  
+  
+  puts "#{download} was retrieved"
+  
+  # Delete the image
+  get = Net::HTTP::Delete.new('/forms/66/1')
+  get_resp = http.request(get)
+  
+  expect(get_resp.code).to eql "404" #delete was successful
+  
+  puts "#{download} was deleted"
+  puts get_resp.code
+  
+  # Try to Retrieve the deleted image
+  get = Net::HTTP::Get.new('/forms/66/1')
+  get_resp = http.request(get)
+  
+  expect(get_resp.code).to eql "404"
+  puts "#{download} was deleted was retrieved"
+  #download = Digest::SHA1.hexdigest(get_resp.body)
+  
+  puts get_resp.code
+  #File.open("output.jpg", 'wb' ) do |output|
+  #output.write get_resp.body
+  #end
+  
+  
+  
+  f = IO.binread('test.tiff')
+
+
+  uri = URI("http://localhost:5006")
+  http = Net::HTTP.new(uri.host, uri.port)
+  
+  # Save the image to the API
+  request = Net::HTTP::Post.new('/forms/A4')
+  request['Content-Type'] = 'image/tiff'
+  request.body = f
+  upload = Digest::SHA1.hexdigest(f)
+  response = http.request(request)
+  
+  puts response.code
+  expect(response.code).to eql "201"
+
+  puts "#{upload} was uploaded as a new document"
+  
+  
+
+  # Replace the image
+  
+  #recall image
   get = Net::HTTP::Get.new('/forms/66')
   get_resp = http.request(get)
   
@@ -37,83 +101,25 @@ When(/^document is added retrieved deleted replaced the system returns a positiv
     output.write get_resp.body
   end
   
-  
-  puts "#{download} was downloaded"
-  
-  # Delete the image
-  get = Net::HTTP::Delete.new('/forms/66/images/2')
-  get_resp = http.request(get)
-  
-  expect(get_resp.code).to eql "200" #delete was successful
-  
-  puts "#{download} was deleted"
-  puts get_resp.code
-  
-  # Try to Retrieve the deleted image
-  get = Net::HTTP::Get.new('/forms/66/images/2')
-  get_resp = http.request(get)
-  
-  expect(get_resp.code).to eql "500"
-
-  #download = Digest::SHA1.hexdigest(get_resp.body)
-  
-  puts get_resp.code
-  #File.open("output.jpg", 'wb' ) do |output|
-  #output.write get_resp.body
-  #end
-  
-  
-  
-  uri = URI("http://localhost:5014")
-  http = Net::HTTP.new(uri.host, uri.port)
-  
-  # Save the image to the API
-  request = Net::HTTP::Post.new('/forms/66/images')
-  request['Content-Type'] = 'image/tiff'
-  request.body = f
-  upload = Digest::SHA1.hexdigest(f)
-  response = http.request(request)
-  
-  puts response.code
-  expect(response.code).to eql "201"
-
-
-  # Replace the image
-  
-  #recall image
-  get = Net::HTTP::Get.new('/forms/66/images/2')
-  get_resp = http.request(get)
-  
-  expect(get_resp.code).to eql "200"
-
-  download = Digest::SHA1.hexdigest(get_resp.body)
-  
-  puts get_resp.code
-  File.open("output.jpg", 'wb' ) do |output|
-    output.write get_resp.body
-  end
-  
  
-  puts "#{download} was downloaded"
+  puts "#{download} was downloaded ready for replace"
   
   f = IO.binread('img2_1.jpeg')
 
   
   # replace the image to the API
-  request = Net::HTTP::Put.new('/forms/66/images/2')
+  request = Net::HTTP::Put.new('/forms/35/1/A4')
   request['Content-Type'] = 'image/jpeg'
   request.body = f
   upload = Digest::SHA1.hexdigest(f)
   response = http.request(request)
-  
   puts response.code
-  expect(response.code).to eql "201"
+  expect(response.code).to eql "200"
   
-  puts "#{upload} was uploaded"
-  puts "#{download} was downloaded"
+  puts "#{upload} is the replacement"
   
   #recall replacement image
-  get = Net::HTTP::Get.new('/forms/66/images/2')
+  get = Net::HTTP::Get.new('/forms/66')
   get_resp = http.request(get)
   
   expect(get_resp.code).to eql "200"
