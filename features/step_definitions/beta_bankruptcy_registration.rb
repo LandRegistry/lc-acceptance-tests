0f838b381ffd0cc3e78eb27582fea06f80c5f99e
Nicola Andrews <nicola.andrews@digital.landregistry.gov.uk>
2016-02-29 10:14:13 +0000

    registration tests updated

--- features/step_definitions/beta_bankruptcy_registration.rb
+++ features/step_definitions/beta_bankruptcy_registration.rb
@@ -125,3 +125,15 @@ end
 When(/^I can click the complete bankruptcy button$/) do
   click_button('continue')
 end
+
+When(/^I enter the specific court details$/) do
+   fill_in('court', :with => 'Plympton County Court')
+   fill_in('ref_no', :with => '111')
+   fill_in('ref_year', :with => '2016')
+   click_button('continue')
+end
+
+Then(/^the registered names are displayed on the screen$/) do
+  expect(page).to have_content('John Smith')
+  expect(page).to have_content('John Alan Smithe')
+end
