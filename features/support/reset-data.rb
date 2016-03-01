1691c5c38fd031c652a3465c5030597bfba205a1
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-03-01 08:26:06 +0000

    Account for spaces...

--- features/support/reset-data.rb
+++ features/support/reset-data.rb
@@ -38,16 +38,16 @@ def execute(clear, setup, save = false, quiet = false)
 
     folder = File.expand_path("../..", File.dirname(__FILE__))
     if clear
-        out = `ruby #{folder}/data/lc-clear.rb`
+        out = `ruby "#{folder}/data/lc-clear.rb"`
         puts "  #{out}" unless(quiet)
-        out = `ruby #{folder}/data/cw-clear.rb`
+        out = `ruby "#{folder}/data/cw-clear.rb"`
         puts "  #{out}" unless(quiet)
     end
 
     if setup
-        out = `ruby #{folder}/data/lc-setup.rb`
+        out = `ruby "#{folder}/data/lc-setup.rb"`
         puts "  #{out}" unless(quiet)
-        out = `ruby #{folder}/data/cw-setup.rb`
+        out = `ruby "#{folder}/data/cw-setup.rb"`
         puts "  #{out}" unless(quiet)
     end
 
