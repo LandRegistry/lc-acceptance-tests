c33b4e342f395f8ce54ff4229e91fb31d8309c04
EricA-G <eric.armah-godwin@digital.landregistry.gov.uk>
2016-03-31 11:15:26 +0100

    cancellation test amended

--- features/step_definitions/cancellation_application_image.rb
+++ features/step_definitions/cancellation_application_image.rb
@@ -186,7 +186,7 @@ expect(page).to have_content("Retrieve original")
 end
 
 Then(/^I can see text to confirm the cancellation$/) do
-expect(find(:id, 'regn_error').text).to eq "Registration has been cancelled - please re-enter"
+expect(find(:id, 'regn_error').text).to have_content("Registration has been cancelled - please re-enter")
 end
 
 Then(/^I can see successful cancellation registration number$/) do
